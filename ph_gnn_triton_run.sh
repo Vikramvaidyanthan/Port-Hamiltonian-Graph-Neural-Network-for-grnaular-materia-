ada56e583bd741ed318b07668c3b899cf1e94eea
Vikramvaidyanthan <151823771+Vikramvaidyanthan@users.noreply.github.com>
2026-06-19 10:54:36 +0300

    Update ph_gnn_triton_run.sh
    Changed error in code

--- ph_gnn_triton_run.sh
+++ ph_gnn_triton_run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=ph_gnn_eval
+#SBATCH --job-name=ph_gnn_train
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=06:00:00
-#SBATCH --output=/scratch/work/venkatv1/ph_gnn_outputs/logs/ph_gnn_eval_%j.out
-#SBATCH --error=/scratch/work/venkatv1/ph_gnn_outputs/logs/ph_gnn_eval_%j.err
+#SBATCH --time=36:00:00
+#SBATCH --output=/scratch/work/venkatv1/ph_gnn_outputs/logs/ph_gnn_%j.out
+#SBATCH --error=/scratch/work/venkatv1/ph_gnn_outputs/logs/ph_gnn_%j.err
 
 # GPU
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu-a100-80g,gpu-v100-32g
 
+
 ROOT_DIR="/scratch/work/venkatv1"
-SCRIPT="${ROOT_DIR}/ph_gnn/ph_gnn_eval_zigzag.py"
+SCRIPT="${ROOT_DIR}/ph_gnn_triton_v2.py"
 OUT_DIR="${ROOT_DIR}/ph_gnn_outputs"
 
 mkdir -p "${OUT_DIR}/logs"
@@ -27,8 +28,7 @@ module load triton/2025.1-gcc
 module load mamba
 
 source ~/.bashrc
-source activate myenv
-
+source activate /scratch/work/venkatv1/envs/ph_gnn_env
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
@@ -38,12 +38,8 @@ export MPLCONFIGDIR="${MPLCONFIGDIR:-/tmp/matplotlib}"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 exec python -u "${SCRIPT}" \
-  --urdf          "${ROOT_DIR}/dataset/spot.urdf" \
-  --dataset_root  "${ROOT_DIR}/Dataset" \
-  --test_subdir   "DatasetTest_zigzag" \
-  --model_path    "${OUT_DIR}/model_best.pt" \
-  --out_dir       "${OUT_DIR}" \
+  --urdf "${ROOT_DIR}/Dataset/spot.urdf" \
   --wandb_project "ph-gnn-phase1" \
-  --wandb_run     "zigzag_eval_$(date +%Y%m%d_%H%M%S)"
+  --wandb_run "triton_a100_run1"
 
 date
